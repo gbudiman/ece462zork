d409482707e2707de10969ffc8c19d753b5a32e5
wahyu.g <wahyu.g@1423a517-4d9d-59f1-cb17-49b7691070be>
2010-09-07 00:57:13 +0000

    Initial commit

---
+++ src/zorkItem.java
@@ -0,0 +1,27 @@
+public class zorkItem extends mapComponent {
+	public String writing = null;
+	public String status = null;
+	public zorkTurnOn turnon = null;
+	public zorkTrigger trigger = null;
+	
+	public zorkItem(String n, String t, String w, String s, zorkTurnOn to, zorkTrigger zt) {
+		super(n, t);
+		writing = w;
+		status = s;
+		turnon = to;
+		zt = trigger;
+	}
+	
+	public void info() {
+		System.out.println(super.type + " " + super.name+ " has properties:");
+		System.out.println(">> Writing: " + writing);
+		System.out.println(">> Status: " + status);
+		if (turnon != null) {
+			turnon.info();
+		}
+		if (trigger != null) {
+			trigger.info();
+		}
+		System.out.println("=================");
+	}
+}
---
+++ src/zorkTrigger.java
@@ -0,0 +1,30 @@
+public class zorkTrigger {
+	public String type;
+	public String command;
+	public zorkCondition condition;
+	public String description = null;
+	public String[] action = null;
+	public boolean hasBeenInvoked = false;
+	
+	public zorkTrigger(String t, String c, zorkCondition zc, String d, String[] a) {
+		type = t;
+		command = c;
+		condition = zc;
+		description = d;
+		action = a;
+	}
+	public void info() {
+		System.out.println("***** TRIGGER *****");
+		System.out.println("Type: " + type);
+		System.out.println("Command: " + command);
+		System.out.print("Action: ");
+		for (int i = 0; i < action.length; i++) {
+			if (action[i] != null) {
+				System.out.print(" [" + action[i] + "]");
+			}
+		}
+		System.out.println();
+		System.out.println("Print: " + description);
+		condition.info();
+	}
+}
